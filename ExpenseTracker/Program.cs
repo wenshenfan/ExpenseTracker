05c5447f300a13055ca269c6b181b8fa61055c19
wenshenfan <jimmy38880@gmail.com>
2024-08-04 19:10:58 +0800

    資料庫初始化

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,7 +1,11 @@
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
