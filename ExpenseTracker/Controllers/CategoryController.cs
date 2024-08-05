fbe4478ef229785489590ef7bc998e8421bf9677
wenshenfan <jimmy38880@gmail.com>
2024-08-05 23:31:54 +0800

    刪除delete view 與get

--- ExpenseTracker/Controllers/CategoryController.cs
+++ ExpenseTracker/Controllers/CategoryController.cs
@@ -75,23 +75,6 @@ namespace ExpenseTracker.Controllers
 
 
 
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
--- ExpenseTracker/Models/Category.cs
+++ ExpenseTracker/Models/Category.cs
@@ -7,12 +7,20 @@ namespace ExpenseTracker.Models
     {
         [Key]
         public int CategoryId { get; set; }
+
         [Column(TypeName="nvarchar(50)")]
-        public string Title {  get; set; }
+        public string Title { get; set; }
         [Column(TypeName = "nvarchar(5)")]
         public string Icon { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
-
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon +" "+ this.Title;
+            }
+        }
     }
 }
